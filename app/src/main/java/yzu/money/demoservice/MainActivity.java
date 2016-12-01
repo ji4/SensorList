4281e43aab56cf16dde04ac57b9301d22c840b36
howdyhi <money@161-17.wlan.yzu.edu.tw>
2016-12-01 15:29:04 +0800

    sensor

--- app/src/main/java/yzu/money/demoservice/MainActivity.java
+++ app/src/main/java/yzu/money/demoservice/MainActivity.java
@@ -1,13 +1,21 @@
 package yzu.money.demoservice;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button btnStart, btnStop;
+    Button btnTestSensor;
+    SensorManager sensorManager;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent it =new Intent(MainActivity.this,MyService.class);
                 startService(it);
+            }
+        });
+
+        //---------------------------------------
+        btnTestSensor= (Button) findViewById(R.id.button3);
+        btnTestSensor.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                sensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
+                List lsensor=sensorManager.getSensorList(Sensor.TYPE_ALL);
+                tv= (TextView) findViewById(R.id.textView);
+                tv.setText(lsensor.size());
             }
         });
     }
