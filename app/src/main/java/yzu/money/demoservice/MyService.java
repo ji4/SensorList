7318ddf2c14ff1768f723e07c03f256285203f16
howdyhi <money@161-17.wlan.yzu.edu.tw>
2016-12-01 14:35:51 +0800

    add onDestroy in MyService

--- app/src/main/java/yzu/money/demoservice/MyService.java
+++ app/src/main/java/yzu/money/demoservice/MyService.java
@@ -19,6 +19,12 @@ public class MyService extends Service {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(show);
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
