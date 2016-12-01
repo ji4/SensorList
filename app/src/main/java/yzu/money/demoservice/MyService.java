57847753bfb9b1dc40fae10d046eb8a3e6be3d4f
howdyhi <money@161-17.wlan.yzu.edu.tw>
2016-12-01 14:29:47 +0800

    handler

--- app/src/main/java/yzu/money/demoservice/MyService.java
+++ app/src/main/java/yzu/money/demoservice/MyService.java
@@ -2,10 +2,20 @@ package yzu.money.demoservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
+
+import java.util.Date;
 
 public class MyService extends Service {
     public MyService() {
+    }
+    Handler handler=new Handler();
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handler.postDelayed(show,1000);
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -13,4 +23,11 @@ public class MyService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+    Runnable show=new Runnable() {
+        @Override
+        public void run() {
+            Log.i("time", new Date().toString());
+            handler.postDelayed(this,1000);
+        }
+    };
 }
