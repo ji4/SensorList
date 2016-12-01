48d7fa213456843d937efa14b4f420b12453858f
howdyhi <money@161-17.wlan.yzu.edu.tw>
2016-12-01 14:04:11 +0800

    add activity

---
+++ app/src/main/java/yzu/money/demoservice/MyService.java
@@ -0,0 +1,16 @@
+package yzu.money.demoservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
