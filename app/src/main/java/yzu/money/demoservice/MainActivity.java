104e3a69d2ffa2aa4880852954cc39d4766363e9
howdyhi <money@AnonymousdeMacBook-Air.local>
2017-01-18 13:29:23 +0800

    show sensor list

--- app/src/main/java/yzu/money/demoservice/MainActivity.java
+++ app/src/main/java/yzu/money/demoservice/MainActivity.java
@@ -2,20 +2,25 @@ package yzu.money.demoservice;
 
 import android.content.Intent;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity/* implements SensorEventListener*/{//imelemnt介面
     Button btnStart, btnStop;
     Button btnTestSensor;
     SensorManager sensorManager;
     TextView tv;
+    //SensorEventListener sel=new SensorEventListener() {
+    //}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,10 +49,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 sensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
-                List lsensor=sensorManager.getSensorList(Sensor.TYPE_ALL);
+                List<Sensor> lsensor=sensorManager.getSensorList(Sensor.TYPE_ALL);
                 tv= (TextView) findViewById(R.id.textView);
-                tv.setText(lsensor.size());
+                tv.setText(String.valueOf(lsensor.toString()));
+                System.out.println(lsensor.toString());
+                /*Sensor s=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                if(s!=null)
+                    sensorManager.registerListener(this,s,SensorManager.SENSOR_DELAY_NORMAL);
+                //sensorManager.registerListener(this,lsensor.get(0),SensorManager.SENSOR_DELAY_NORMAL);
+                sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);*/
             }
         });
     }
+
+    /*@Override
+    public void onSensorChanged(SensorEvent event) {
+        float []v=event.values;
+        Log.i("test",String.valueOf(v[0]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }*/
 }
